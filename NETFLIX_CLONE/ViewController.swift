6b61d0c35247e747d67d1371f235a862a7d4b699
89Explorer <leral3@naver.com>
2024-07-03 07:32:36 +0900

    Initial Commit

---
+++ NETFLIX_CLONE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NETFLIX_CLONE
+//
+//  Created by 권정근 on 7/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
